61000a0e93aa9e544c93af524cc8158db406edbf
mohammadakeeb0011 <mohammadakeeb786@gmail.com>
2023-09-30 12:59:10 +0530

    command line arguments

---
+++ C_Programs/10_Command_Line_Arguments/reading_file.c
@@ -0,0 +1,45 @@
+/***************************************************
+ * This program reads a file specified by the user *
+ * as a command line argument and display the      *
+ * contents of the file on screen.                 *
+ ***************************************************/
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+ FILE *fp;
+ char *filename;
+ char ch;
+
+  // Check if a filename has been specified in the command
+  if (argc < 2)
+   {
+        printf("Missing Filename\n");
+        return(1);
+   }
+   else
+  {
+        filename = argv[1];
+        printf("Filename : %s\n", filename);
+   }
+
+   // Open file in read-only mode
+   fp = fopen(filename,"r");
+
+   // If file opened successfully, then print the contents
+   if ( fp )
+      {
+        printf("File contents:\n");
+        while ( (ch = fgetc(fp)) != EOF )
+           {
+                printf("%c",ch);
+           }
+
+       }
+   else
+      {
+         printf("Failed to open the file\n");
+        }
+
+return(0);
+}
