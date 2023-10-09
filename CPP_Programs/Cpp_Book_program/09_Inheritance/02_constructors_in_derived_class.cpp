fc60762bb88f198961502f5f3b1e6c3f1650885c
mohammadakeeb0011 <mohammadakeeb786@gmail.com>
2023-10-09 23:39:17 +0530

    09/10/2023

---
+++ CPP_Programs/Cpp_Book_program/09_Inheritance/02_constructors_in_derived_class.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+using namespace std;
+
+class Counter{
+    protected:
+        unsigned int count;
+    public:
+        Counter():count(0)
+        {        }
+        Counter(int n):count(n)
+        {        }
+        unsigned int get_count() const
+        {   return count;   }
+        Counter operator ++ ()
+        { return Counter(++count); }
+};
+
+class CountDn: public Counter{
+    public:
+        CountDn():Counter()
+        {}
+        CountDn(int c): Counter(c)
+        {}
+        CountDn operator -- ()
+        { return CountDn(--count);}
+        CountDn operator -- (int)
+        { return CountDn(--count);}
+};
+
+int main()
+{
+    CountDn c1;
+    CountDn c2(100);
+
+    cout<<"\nc1 ="<< c1.get_count();
+    cout<<"\nc2 ="<< c2.get_count();
+
+    ++c1; ++c1; ++c1;
+    cout<<"\nc1 ="<< c1.get_count();
+
+    --c2, --c2;
+    cout<<"\nc2 ="<< c2.get_count();
+
+    CountDn c3 = --c2;
+    cout<<"\nc3 ="<< c3.get_count();
+
+    CountDn c4 = c3--;
+    cout<<"\nc4 ="<< c4.get_count();
+    cout<<endl;
+    return 0;
+}
